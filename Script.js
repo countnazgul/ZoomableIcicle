4fe3f47092d3410699226e853b611d9f44b136b9
Stefan Stoichev <stefan.stoichev@gmail.com>
2015-10-04 05:44:09 +0100

    v0.3.0
    * show/hide labels based on the rect size
    * code cleanup

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 // Zoomable Icicle Qlikview Extension
 // Author: stefan.stoichev@gmail.com
-// Version: 0.5.0
+// Version: 0.3.0
 // Repo: https://github.com/countnazgul/ZoomableIcicle
 // d3 example used: http://bl.ocks.org/mbostock/1005873
 // Thanks to: Cynthia Brewer for the ColorBrewer Scale http://bl.ocks.org/mbostock/5577023
@@ -65,8 +65,8 @@ function extension_Done(){
     var zoomSpeed       = parseInt(_this.Layout.Text5.text.toString());
     var Width           = parseInt(_this.Layout.Text6.text.toString());
     var Height          = parseInt(_this.Layout.Text7.text.toString());
-	var BorderWidth          = parseFloat(_this.Layout.Text8.text.toString());
-	var BorderColor          = _this.Layout.Text9.text.toString();
+    var BorderWidth          = parseFloat(_this.Layout.Text8.text.toString());
+    var BorderColor          = _this.Layout.Text9.text.toString();
     var showValues = false;
         var width = Width,
         height = Height;
@@ -192,160 +192,72 @@ function extension_Done(){
             .range( colorbrewer[colorScheme][colorSchemeNo])
      ;
      
-     var color = d3.scale.category20c();
-   var x_range = d3.scale.linear().range([0, width]),
-    y_range = d3.scale.linear().range([0, height])
-// var partition = d3.layout.partition()
-//     .children(function(d) { return isNaN(d.value) ? d3.entries(d.value) : null; })
-//     .value(function(d) { return d.value; });
-
-    // var partition = d3.layout.partition()
-    //     .value(function(d) {
-		// 		return d.size;
-		// })
-    
-
-var partition = d3.layout.partition()
-    //.children(function(d) { return isNaN(d.children) ? d3.entries(d.children) : null; })
-    .value(function(d) { return d.size; });    
+    var color = d3.scale.category20c();
+    var x_range = d3.scale.linear().range([0, width]);
+    var y_range = d3.scale.linear().range([0, height]);
+    var partition = d3.layout.partition()    
+      .value(function(d) { return d.size; });    
             
     var svg = d3.select("#" + divName).append("svg")
         .attr("width", width)
         .attr("height", height)
-//        .append("g")
-//        .attr("transform", "translate(" + width / 2 + "," + (height / 2 + 10) + ")")
-;    
-      var rect = svg.selectAll("rect");
+    ;
+        
+    var rect = svg.selectAll("rect");
+    var root = nodesJson;  
+    var data = partition.nodes( root );
 
-        var root = nodesJson;
-        //console.log( JSON.stringify( nodesJson ) )     
-     
-  // rect = rect
-  //    .data(partition.nodes( root )) //d3.entries(root)[0]
-  //   .enter().append("rect")
-  //     .attr("x", function(d) { return x(d.x); })
-  //     .attr("y", function(d) { return y(d.y); })
-  //     .attr("width", function(d) { return x(d.dx); })
-  //     .attr("height", function(d) { return y(d.dy); })
-  //     .attr("fill", function(d) { return color((d.children ? d : d.parent).name); })
-  //     .on("click", clicked)
-  
-  var data = partition.nodes( root );
-  console.log(data)
-      var rect = svg.selectAll("rect").data(data).enter()
-     .append("svg:rect")
-     .attr("x", function(d) { return x_range(d.x); })
-     .attr("y", function(d) { return y_range(d.y); })
-     .attr("width", function(d) { return x_range(d.dx); })
-     .attr("height", function(d) { return y_range(d.dy); })
-    .attr("fill", function(d) {return color(d.name);})
-     //.style("cursor", "pointer")
-     .on("click", clicked);
-     
-    var fo = svg.selectAll("text").data(data).enter()
-     .append("svg:text")
-     .attr("x", function(d) { return x_range(d.x); })
-     .attr("y", function(d) { return y_range(d.y +0.03); })
-     .attr("width", function(d) { return x_range(d.dx); })
-     .attr("height", function(d) { return y_range(d.dy); })
-     .style("cursor", "pointer")
+      var g = svg.selectAll("g")
+          .data( data )
+          .enter().append("g")
+        ;
+
+    var rect = g.append("rect")
+    .attr("x", function(d) { return x_range(d.x); })
+    .attr("y", function(d) { return y_range(d.y); })
+    .attr("width", function(d) { return x_range(d.dx); })
+    .attr("height", function(d) { return y_range(d.dy); })
+    .attr("fill", function(d) {   return color(d.name);})
+    .attr("bb", function(d) {        
+      d.bb = this.getBBox().width;       
+      return this.getBBox().width })
+    .style("cursor", "pointer")
+    .on("click", clicked);
+    
+    var fo = g.append("text")
+    .attr("x", function(d) { return x_range(d.x); })
+    .attr("y", function(d) { return y_range(d.y +0.03); })
+    .attr("width", function(d) { return x_range(d.dx); })
+    .attr("height", function(d) { return y_range(d.dy); })
+    .style("cursor", "pointer")
+    .attr("font-size", 14)
     .text(function(d) { return d.name })
-     .on("click", clicked);
-
- function clicked(d) {
-  x_range.domain([d.x, d.x + d.dx]);
-  y_range.domain([d.y, 1]).range([d.y ? 20 : 0, height]);
-
-  rect.transition()
-      .duration(750)
-            .attr("x", function(d) { return x_range(d.x); })
-            .attr("y", function(d) { return y_range(d.y); })
-            .attr("width", function(d) { return x_range(d.x + d.dx) - x_range(d.x); })
-            .attr("height", function(d) { return y_range(d.y + d.dy) - y_range(d.y); });
-      
-
-        fo.transition()
-            .duration(750)
-            .attr("x", function(d) { return x_range(d.x); })
-            .attr("y", function(d) { return y_range(d.y + 0.03); })
-            .attr("width", function(d) { return x_range(d.x + d.dx) - x_range(d.x); })
-            .attr("height", function(d) { return y_range(d.y + d.dy) - y_range(d.y); });      
-      
-}   
-
-          //console.log( JSON.stringify( $( '#' +divName ) ) )
-     
-
-// 		// var arc = d3.svg.arc()
-//     //     .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
-//     //     .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
-//     //     .innerRadius(function(d) { return Math.max(0, y(d.y)); })
-//     //     .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); })
-// 		// ;
-
-
-
-//       // var g = svg.selectAll("g")
-//       //     .data(partition.nodes(root))
-//       //     .enter().append("g")
-//       //   ;
-
-
-
-
-      // var text = g.append("text")
-      //   .attr("transform", function(d) { return "rotate(" + computeTextRotation(d) + ")"; })
-      //   .attr("x", function(d) { return y(d.y); })
-      //   .attr("dx", "6") // margin
-      //   .attr("dy", ".35em") // vertical-align
-      //   .attr("font-size", fontSize)
-      //   .attr("font-family", fontFamily)
-      //   .text(function(d) { return d.name })
-      //   .attr("visibility",function(d) { return d.dx < 0.01? "hidden" : "visible"})
-      //   ;
-
-      // function click(d) {
-      //   var total = d.dx;
-      //   // fade out all text elements
-      //   text.transition().attr("opacity", 0);
-
-      //   path.transition()
-      //     .duration(zoomSpeed)
-      //     .attrTween("d", arcTween(d))
-      //     .each("end", function(e, i) {
-      //         // check if the animated element's data e lies within the visible angle span given in d
-      //         if (e.x >= d.x && e.x < (d.x + d.dx)) {
-      //           // get a selection of the associated text element
-      //           var arcText = d3.select(this.parentNode).select("text");
-      //           // fade in the text element and recalculate positions
-      //           arcText.transition().duration(750)
-      //             .attr("opacity", 1)
-
-      //             .attr("transform", function() { return "rotate(" + computeTextRotation(e) + ")" })
-      //             .attr("x", function(d) { return y(d.y); })
-      //             .attr("visibility",function(d) { return d.dx/total < 0.01? "hidden" : "visible"});
-      //         }
-      //     });
-      // }
-
-    // d3.select(self.frameElement).style("height", height + "px");
-
-    // Interpolate the scales!
-    // function arcTween(d) {
-    //   var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
-    //       yd = d3.interpolate(y.domain(), [d.y, 1]),
-    //       yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
-    //   return function(d, i) {
-    //     return i
-    //         ? function(t) { return arc(d); }
-    //         : function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
-    //   };
-    // }
-
-    // function computeTextRotation(d) {
-    //   return (x(d.x + d.dx / 2) - Math.PI / 2) / Math.PI * 180;
-    // }
-  //});
+    .attr("opacity",function(d) {  d.w = this.getComputedTextLength(); return d.bb > d.w  ? 1 : 0})
+    .on("click", clicked);
+  
+    function clicked(d) {
+      x_range.domain([d.x, d.x + d.dx]);
+      y_range.domain([d.y, 1]).range([d.y ? 20 : 0, height]);
+    
+      rect.transition()
+          .duration(750)
+                .attr("x", function(d) { return x_range(d.x); })
+                .attr("y", function(d) { return y_range(d.y); })
+                .attr("width", function(d) { d.bb = x_range(d.x + d.dx) - x_range(d.x); return x_range(d.x + d.dx) - x_range(d.x); })
+                .attr("height", function(d) { return y_range(d.y + d.dy) - y_range(d.y); })
+      ;
+                
+      fo.transition()
+          .duration(750)
+          .attr("x", function(d) { return x_range(d.x); })
+          .attr("y", function(d) { return y_range(d.y + 0.03); })
+          .attr("width", function(d) { return x_range(d.x + d.dx) - x_range(d.x); })
+          .attr("height", function(d) { return y_range(d.y + d.dy) - y_range(d.y); })
+          .attr("opacity",function(d) {  
+              d.w = this.getComputedTextLength(); 
+              return d.bb > d.w  ? 1 : 0 })   
+      ;    
+    }
   })
 }
 
